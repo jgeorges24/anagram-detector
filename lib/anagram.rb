919fbbed09af4b84c2fef91abd2b37831ab21cc2
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-02-13 06:07:56 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+def match
+end
+    
+def allergy
+end
+
+def detector
+end
+
+end
+
+daddy_yankee = Anagram.new
+
+def daddy_yankee.match
+
+    puts "#{} This is a match"
+
+end
